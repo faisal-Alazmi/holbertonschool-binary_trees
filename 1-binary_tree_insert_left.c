2cf34f204c290aa83eeeb69dc266ea219ea4a321
root <root@ip-10-42-144-27.ec2.internal>
2025-10-31 22:30:31 +0000

    Add tasks 0 to 6: binary tree basic functions

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -2,12 +2,12 @@
 #include "binary_trees.h"
 
 /**
-	* binary_tree_insert_right - Inserts a node as the right-child of another node
-	* @parent: Pointer to the node to insert the right-child in
+	* binary_tree_insert_left - Inserts a node as the left-child of another node
+	* @parent: Pointer to the node to insert the left-child in
 	* @value: Value to store in the new node
 	* Return: Pointer to the created node, or NULL on failure
 	*/
-binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new;
 
@@ -20,13 +20,13 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 
 	new->n = value;
 	new->parent = parent;
-	new->right = parent->right;
-	new->left = NULL;
+	new->left = parent->left;
+	new->right = NULL;
 
-	if (parent->right != NULL)
-	parent->right->parent = new;
+	if (parent->left != NULL)
+	parent->left->parent = new;
 
-	parent->right = new;
+	parent->left = new;
 
 	return (new);
 }
